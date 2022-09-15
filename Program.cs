fb36cf78ab234501ff4766f17fc30487938a1539
Victoriya <victory_alex@mail.ru>
2022-09-16 00:19:40 +0300

    Программа вывода массива строк длина которых меньше или равна 3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string [] array = {"hellow", "2", "world", ":-)", "Russia", "Kazan","-2"};
+Console.WriteLine($"Задан массив: {String.Join(", ",  array)}");
+Console.WriteLine();
+Console.WriteLine("Новый массив из строк длина которых меньше или равна 3:");
+CreateArray(array);
+
+void CreateArray (string [] newArray)
+{   string [] array = new string [newArray.Length];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length<=3)
+        {
+            array[i]=newArray[i];
+            Console.Write($"{newArray[i]} ");
+        }
+    }
+}
+
+
+
+
+
